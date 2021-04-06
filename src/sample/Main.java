17f3da4542d36d1fc1acc18ca9cf838e311d1791
Wahbi <wahbi.raihan@ontariotechu.net>
2021-04-06 16:23:26 -0400

    Set up buttons and pane

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,18 +2,83 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    //Hello friends, I don't like fxml so I am going to fuck up the ui and make it here, this code will look ugly, sorry in advance!
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        //Set up BorderPane
+        BorderPane root = new BorderPane();
+
+        //Declare buttons
+        Button searchButton = new Button("Search for match");
+        searchButton.setMinWidth(200);
+        searchButton.setMinHeight(100);
+        Button exitButton = new Button("Exit");
+        exitButton.setMinWidth(200);
+        exitButton.setMinHeight(100);
+
+
+
+        //Set up button
+        searchButton.setOnAction(e -> searchButtonPress());
+        exitButton.setOnAction(e -> exitButtonPress());
+
+        //Setting up top panel
+        GridPane topPanel = new GridPane();
+        topPanel.setAlignment(Pos.TOP_LEFT);
+        topPanel.setHgap(10);
+        topPanel.setVgap(10);
+        topPanel.setPadding(new Insets(5, 0, 5, 0));
+
+
+        //Set up bottom panel
+        GridPane bottomPanel = new GridPane();
+        bottomPanel.setAlignment(Pos.BOTTOM_LEFT);
+        bottomPanel.setHgap(10);
+        bottomPanel.setVgap(10);
+        bottomPanel.setPadding(new Insets(5, 0, 5, 0));
+
+        //Adding buttons to panel
+        topPanel.add(searchButton, 0, 0);
+        bottomPanel.add(exitButton, 70, 0);
+
+        //Adding Panels to BorderPane
+        root.setTop(topPanel);
+        root.setBottom(bottomPanel);
+
+        //Setting up scene
+        primaryStage.setScene(new Scene(root, 900, 800));
         primaryStage.show();
+    }
+
+    private void exitButtonPress() {
+        System.exit(1);
+    }
+
+    private void searchButtonPress() {
+        Stage stage = new Stage();
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        Text errorMessage = new Text("DOES LITERALLY NOTHING YOU DENT");
+        grid.add(errorMessage, 0, 0);
+        Scene tempScene = new Scene(grid, 300, 100);
+        stage.setScene(tempScene);
+        stage.show();
+        System.out.println("This does literally nothing yet, go add something you dent");
     }
 
 
